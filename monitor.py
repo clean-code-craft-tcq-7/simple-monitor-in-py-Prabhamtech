e250558a2ba8e88c3cd1d673b8e51c6fd328fb6f
prabha <prabhamtechsrm@gmail.com>
2025-07-31 16:39:46 +0530

    reduced complexity

--- monitor.py
+++ monitor.py
@@ -9,13 +9,24 @@ def blink_alert(message, duration=6):
             sys.stdout.flush()
             sleep(1)
  
-def vitals_ok(temperature, pulse_rate, spo2):
+def vitals_ok(temperature, pulseRate, spo2):
+    if (temperature_ok(temperature) and pulse_rate_ok(pulseRate) and spo2_ok(spo2)):
+        return True
+    return False
+
+def temperature_ok(temperature):
     if temperature > 102 or temperature < 95:
         blink_alert('Temperature critical!')
         return False
-    if pulse_rate < 60 or pulse_rate > 100:
+    return True
+
+def pulse_rate_ok(pulseRate):
+    if pulseRate < 60 or pulseRate > 100:
         blink_alert('Pulse Rate is out of range!')
         return False
+    return True
+
+def spo2_ok(spo2):
     if spo2 < 90:
         blink_alert('Oxygen Saturation out of range!')
         return False
